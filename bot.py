9df2021c820529391b08469518093c2dfe3061c0
Evgeny Skachkov <ESkachkov@sbercity.ru>
2025-03-31 02:18:02 +0800

    some optimization

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import io
 import logging
 import re
 from datetime import datetime, timedelta
+from itertools import repeat
 from os import getenv
 
 from aiogram import Bot, Dispatcher, F, types
@@ -14,12 +15,13 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.storage.redis import RedisStorage
+from aiogram.methods import SendMessage
 from aiogram.types import BufferedInputFile, Message
 from aiogram.utils.markdown import hbold
 
+from gen import generate
 from redis_handlers import init_redis
 from states import UserStates
-from gen import generate
 
 # Set up logging
 LOG_LEVEL = getenv("LOG_LEVEL", "INFO")
@@ -41,6 +43,11 @@ except Exception as e:
 dp = Dispatcher(storage=storage)
 
 
+# Concurently version of the message.answer method
+async def message_answer(message: Message, text: str) -> SendMessage:
+    return await message.answer(text)
+
+
 async def add_points(state: FSMContext) -> None:
     try:
         data = await state.get_data()
@@ -53,10 +60,12 @@ async def add_points(state: FSMContext) -> None:
         _LOGGER.info(
             f"Пользователь {user_name} справился и получил {points} баллов."
         )
-        await state.update_data(difficulty=difficulty, points=points)
+        asyncio.create_task(
+            state.update_data(difficulty=difficulty, points=points)
+        )
     except:
         _LOGGER.error("Ошибка при добавлении баллов")
-        await state.update_data(difficulty=0, points=0)
+        asyncio.create_task(state.update_data(difficulty=0, points=0))
 
 
 async def subtract_points(state: FSMContext) -> None:
@@ -71,27 +80,34 @@ async def subtract_points(state: FSMContext) -> None:
         _LOGGER.info(
             f"Пользователь {user_name} не справился и получил {points} баллов."
         )
-        await state.update_data(difficulty=difficulty, points=points)
+        asyncio.create_task(
+            state.update_data(difficulty=difficulty, points=points)
+        )
     except:
         _LOGGER.error("Ошибка при вычитании баллов")
-        await state.update_data(difficulty=0, points=0)
+        asyncio.create_task(state.update_data(difficulty=0, points=0))
 
 
 async def get_new_task(message: Message, state: FSMContext) -> None:
     data = await state.get_data()
-    difficulty = data.get("difficulty")
+    difficulty = data.get("difficulty", 0)
     expression, answer = generate(difficulty)
-    await state.update_data(
-        expression=expression,
-        answer=answer,
-        user_name=message.from_user.full_name,
+    asyncio.create_task(
+        state.update_data(
+            expression=expression,
+            answer=answer,
+            user_name=message.from_user.full_name,
+        )
     )
-    await message.answer(
-        "🔔 Внимание-внимание! Новый примерчик! 🔔\n"
-        f"<code>{expression}</code>\n"
-        "Скорее пиши ответ! ⏱️"
+    asyncio.create_task(
+        message_answer(
+            message,
+            "🔔 Внимание-внимание! Новый примерчик! 🔔\n"
+            f"<code>{expression}</code>\n"
+            "Скорее пиши ответ! ⏱️",
+        )
     )
-    await state.set_state(UserStates.await_1_answer)
+    asyncio.create_task(state.set_state(UserStates.await_1_answer))
 
 
 @dp.message(CommandStart())
@@ -99,27 +115,31 @@ async def start_handler(message: Message, state: FSMContext) -> None:
     data = await state.get_data()
     difficulty = data.get("difficulty")
     if difficulty is None:
-        await message.answer(
+        await message_answer(
+            message,
             f"Приветик, {hbold(message.from_user.full_name)}! 🌟\n"
             "Я твой весёлый помощник в мире вычислений! 🚀\n\n"
             "Давай начнём с простеньких примеров, а если ты будешь щёлкать их как орешки, "
             "я подкину тебе задачки посложнее! 😉\n"
-            "Со мной будешь суперзвездой математики! 💫"
+            "Со мной будешь суперзвездой математики! 💫",
         )
-        await state.update_data(difficulty=0)
+        asyncio.create_task(state.update_data(difficulty=0))
         _LOGGER.info(
             f"Новый пользователь: {message.from_user.full_name}, id: {message.from_user.id}"
         )
     else:
-        await message.answer(
+        await message_answer(
+            message,
             f"Снова здравствуй, {hbold(message.from_user.full_name)}! 🌈\n"
-            f"Сейчас мы с тобой на уровне сложности: {difficulty} ⚡️"
+            f"Сейчас мы с тобой на уровне сложности: {difficulty} ⚡️",
         )
         _LOGGER.info(
             f"Повторный запуск: {message.from_user.full_name}, id: {message.from_user.id}"
         )
-    await state.update_data(user_name=message.from_user.full_name)
-    await get_new_task(message, state)
+    asyncio.create_task(
+        state.update_data(user_name=message.from_user.full_name)
+    )
+    asyncio.create_task(get_new_task(message, state))
 
 
 @dp.message(Command("stop"))
@@ -127,10 +147,13 @@ async def stop_handler(message: Message, state: FSMContext) -> None:
     try:
         await state.clear()
     finally:
-        await message.answer(
-            f"Пока-пока, {hbold(message.from_user.full_name)}! 👋\n"
-            "Я бережно записываю твои успехи... шучу, забыл всё! 🤫\n"
-            "Возвращайся скорее, будем играть ещё! 🎮"
+        asyncio.create_task(
+            message_answer(
+                message,
+                f"Пока-пока, {hbold(message.from_user.full_name)}! 👋\n"
+                "Я бережно записываю твои успехи... шучу, забыл всё! 🤫\n"
+                "Возвращайся скорее, будем играть ещё! 🎮",
+            )
         )
         _LOGGER.info(
             f"Пользователь {message.from_user.full_name} завершил работу с ботом"
@@ -139,44 +162,52 @@ async def stop_handler(message: Message, state: FSMContext) -> None:
 
 @dp.message(UserStates.await_1_answer)
 async def answer1_handler(message: Message, state: FSMContext) -> None:
-    ans = message.text.replace(" ", "").replace(",", ".").replace("=", "-")
     data = await state.get_data()
     try:
+        ans = message.text.replace(" ", "").replace(",", ".").replace("=", "-")
         ans = float(ans)
         right_answer = data.get("answer")
         if ans == right_answer:
-            await message.answer(("🤩"))
-            await message.answer(("🎉 УРА!!! 🎉"))
-            await message.answer(
-                "С ПЕРВОЙ попытки! 🌟\n"
-                "Это точно золотая медаль! 🥇\n"
-                "Следующий пример уже готовится в моей печке... 😄"
+
+            messages = [
+                "🤩",
+                "🎉 УРА!!! 🎉",
+                "С ПЕРВОЙ попытки! 🌟\nЭто точно золотая медаль! 🥇",
+            ]
+            await asyncio.gather(
+                *map(message_answer, repeat(message), messages)
             )
             _LOGGER.info(
                 f"Пользователь {message.from_user.full_name} решил пример с первой попытки"
             )
             await add_points(state)
-            await get_new_task(message, state)
+            asyncio.create_task(get_new_task(message, state))
         else:
-            await message.answer(
-                "Ой-ой-ой! 🫢\n"
-                "Кажется, тут маленькая ошибка! 🧐\n"
-                "Проверь аккуратненько и попробуй ещё раз!\n"
-                "Вот наш пример:\n"
-                f"<code>{data.get('expression')}</code>\n"
-                "Ты обязательно справишься! 💪"
+            asyncio.create_task(
+                message_answer(
+                    message,
+                    "Ой-ой-ой! 🫢\n"
+                    "Кажется, тут маленькая ошибка! 🧐\n"
+                    "Проверь аккуратненько и попробуй ещё раз!\n"
+                    "Вот наш пример:\n"
+                    f"<code>{data.get('expression')}</code>\n"
+                    "Ты обязательно справишься! 💪",
+                )
             )
-            await state.set_state(UserStates.await_2_answer)
+            asyncio.create_task(state.set_state(UserStates.await_2_answer))
             _LOGGER.info(
                 f"Пользователь {message.from_user.full_name} ошибся первый раз"
             )
     except:
-        await message.answer(
-            "Циферки-циферки! 🔢\n"
-            "Давай только числа, как настоящие математики! 🧮\n"
-            "Попробуй ещё разок! 😊\n"
-            "Вот наш пример:\n"
-            f"<code>{data.get('expression')}</code>\n"
+        asyncio.create_task(
+            message_answer(
+                message,
+                "Циферки-циферки! 🔢\n"
+                "Давай только числа, как настоящие математики! 🧮\n"
+                "Попробуй ещё разок! 😊\n"
+                "Вот наш пример:\n"
+                f"<code>{data.get('expression')}</code>\n",
+            )
         )
         return
 
@@ -187,33 +218,38 @@ async def answer2_handler(message: Message, state: FSMContext) -> None:
     data = await state.get_data()
     try:
         ans = float(ans)
-        reight_answer = data.get("answer")
-        if ans == reight_answer:
-            await message.answer(
+        right_answer = data.get("answer")
+        if ans == right_answer:
+            await message_answer(
+                message,
                 "🎈 Ура! Получилось! 🎈\n"
                 "Со второй попытки - это круто! 😎\n"
-                "Твоя награда - серебряная медалька! 🥈"
+                "Твоя награда - серебряная медалька! 🥈",
             )
             _LOGGER.info(
                 f"Пользователь {message.from_user.full_name} решил пример со второй попытки"
             )
             await add_points(state)
-            await get_new_task(message, state)
+            asyncio.create_task(get_new_task(message, state))
         else:
-            await message.answer(
+            await message_answer(
+                message,
                 "Хм-м... 🤔\n"
                 "Опять маленькая помарка! 🎨\n"
                 "Давай посмотрим внимательнее:\n"
                 f"<code>{data.get('expression')}</code>\n"
-                "Ты почти у цели! 🌈"
+                "Ты почти у цели! 🌈",
             )
-            await state.set_state(UserStates.await_3_answer)
+            asyncio.create_task(state.set_state(UserStates.await_3_answer))
     except:
-        await message.answer(
-            "Ой, это что ли буквы?... а надо цифры! 🔤➡️🔢\n"
-            "Попробуй написать просто число, как мы договаривались! 🤝\n"
-            "Вот наш пример:\n"
-            f"<code>{data.get('expression')}</code>\n"
+        asyncio.create_task(
+            message_answer(
+                message,
+                "Ой, это что ли буквы?... а надо цифры! 🔤➡️🔢\n"
+                "Попробуй написать просто число, как мы договаривались! 🤝\n"
+                "Вот наш пример:\n"
+                f"<code>{data.get('expression')}</code>\n",
+            )
         )
         return
 
@@ -226,37 +262,45 @@ async def answer3_handler(message: Message, state: FSMContext) -> None:
         ans = float(ans)
         reight_answer = data.get("answer")
         if ans == reight_answer:
-            await message.answer(
+            await message_answer(
+                message,
                 "🎆 ФАНФАРЫ! 🎇\n"
                 "С третьей попытки - это победа! 🏁\n"
-                "Ты настоящий упорный боец! 💥"
+                "Ты настоящий упорный боец! 💥",
             )
             await add_points(state)
-            await get_new_task(message, state)
+            asyncio.create_task(get_new_task(message, state))
         else:
-            await message.answer(("🫂 Не грусти!"))
-            await message.answer(
+            await message_answer(message, "🫂 Не грусти!")
+            await message_answer(
+                message,
                 "Этот пример был слишком хитрющим! 🦊\n"
                 f"Правильный ответ: <b>{reight_answer}</b>\n"
                 "Давай возьмём новый пример - он точно по зубам! 😉\n"
-                "Уже бегу искать... 🏃"
+                "Уже бегу искать... 🏃",
             )
             await subtract_points(state)
-            await get_new_task(message, state)
+            asyncio.create_task(get_new_task(message, state))
     except:
-        await message.answer(
-            "Кажется, кто-то хочет поиграть в загадки? 🎭\n"
-            "Но мне нужно именно число - давай попробуем ещё раз! 🤗\n"
-            "Вот наш пример:\n"
-            f"<code>{data.get('expression')}</code>\n"
+        asyncio.create_task(
+            message_answer(
+                message,
+                "Кажется, кто-то хочет поиграть в загадки? 🎭\n"
+                "Но мне нужно именно число - давай попробуем ещё раз! 🤗\n"
+                "Вот наш пример:\n"
+                f"<code>{data.get('expression')}</code>\n",
+            )
         )
         return
 
 
 @dp.message()
 async def echo_handler(message: types.Message) -> None:
-    await message.answer(
-        "🤖 Я понимаю только числовые ответы, давай играть! 🎲"
+    asyncio.create_task(
+        message_answer(
+            message,
+            "🤖 Я понимаю только числовые ответы, давай играть! 🎲\nДля начала используй команду /start",
+        )
     )
 
 
